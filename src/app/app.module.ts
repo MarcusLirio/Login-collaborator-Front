edebd8a9102f0f1c1464400aa4ccfe7bbb2b587f
vidigaljr <ivan.vidigaljr@gmail.com>
2022-11-29 12:08:11 -0300

    Tela admin JR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AdminComponent } from './layout/admin/admin.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatSidenavModule,
     BrowserAnimationsModule,
-  ],  
+    TableModule
+  ],
   providers: [
     httpInterceptorProviders,
     AuthGuard,
--- src/app/layout/admin/admin.component.ts
+++ src/app/layout/admin/admin.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
+  styleUrls: ['/src/styles.css']
 })
 export class AdminComponent implements OnInit {
 
-    constructor() {}
+  constructor() { }
 
-    ngOnInit() {
-    }
-
- 
+  ngOnInit(): void {
+  }
 }
